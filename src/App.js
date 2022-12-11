a1a6e1daa027a08e90fa5b0f4c815212b236d461
unknown-yfk <fkt2119@gmail.com>
2022-12-11 15:35:44 +0300

    deleted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
-function App() {
+function Apps() {
+
+  const name = "Fike"
+  const x = false
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Apps">
+    
+        <h1>hello{name}</h1>
+        <h2>hello from {x ? 'yes' : 'no'}</h2>
+        <Header title="helloss"/>
     </div>
   );
 }
 
-export default App;
+export default Apps;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+const Header = (props) => {
+    
+    return (
+
+        <header>
+            <h1>Header {props.title}</h1>
+        </header>
+    )
+
+}
+
+export default Header
