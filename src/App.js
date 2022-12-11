1d42d69ab2c379cd5f525dfbd6dace73ef891899
unknown-yfk <fkt2119@gmail.com>
2022-12-11 20:10:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Header from './components/Header'
 function Apps() {
 
   const name = "Fike"
-  const x = false
+  // const x = false
   return (
     <div className="Apps">
     
-        <h1>hello{name}</h1>
-        <h2>hello from {x ? 'yes' : 'no'}</h2>
-        <Header title="helloss"/>
+        <h1 >hello {name}</h1>
+        {/* <h2>hello from {x ? 'yes' : 'no'}</h2> */}
+        <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,43 @@
 
-const Header = (props) => {
+import propTypes from 'prop-types'
+
+const Header = ({ title }) => {
     
     return (
 
         <header>
-            <h1>Header {props.title}</h1>
+            {/* <h1 style={{color:'red', backgroundColor:'black', padding:'20px'}}>Header {title} </h1> */}
+            {/* <h1 style={headingStyle}>Header {title}</h1> */}
+
+                <h1>{title}</h1>
+        
+        
         </header>
     )
 
 }
+// default props
+Header.defaultProps = {
+    title: 'Task Trackers',
+
+}
+
+// defining prop type
+Header.propTypes = {
+
+    title: propTypes.string.isRequired,
+    
+}
+
+// css in javascript
+// const headingStyle = {
+
+//     color: 'red',
+//     backgroundColor:'black',
+//     padding:'20px'
+
+// }
+
+
 
 export default Header
