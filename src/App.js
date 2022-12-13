4b20f8a86d59bff3ebe32552f0b7b37a3fc401c1
unknown-yfk <fkt2119@gmail.com>
2022-12-13 18:09:31 +0300

    hell

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import {  Layout, Menu } from 'antd';
 
 function Apps() {
+  const { Header } = Layout;
   return (
-    <div className = "container">
-      
-      <h1>hello</h1>
-       
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return {
+              key,
+              label: `nav ${key}`,
+            };
+          })}
+        />
+      </Header>
+    </Layout>
   );
 }
 
