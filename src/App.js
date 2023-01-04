5bf48e38d3aa75a521f70071292757d1f28f10c8
unknown-yfk <fkt2119@gmail.com>
2023-01-04 18:51:38 +0300

    added

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
 
       {/* main-content  */}
     <div class="panel-body">
-
+{/* 
           <div class="row">
 
               <hr/>
@@ -55,24 +55,13 @@ const App = () => {
 
             <div class="col-md-7">
               
-              {/* <hr/> */}
+              
            <div class="input-group-sm form-control-div1">
               <input type="text" class="form-control" placeholder="g1_slot1"/>
                <button class="btn btn-danger btn-sm"><i class="fas fa-minus"></i></button>
           </div>
 
-              <hr/>
-           <div class="input-group-sm form-control-div1 mt-5">
-              <input type="text" class="form-control" placeholder="g1_slot2"/>
-               <button class="btn btn-danger btn-sm"><i class="fas fa-minus"></i></button>
-          </div>
-
-            
-              <hr/>        
-            <div class="input-group-sm form-control-div1 mt-5">
-              <input type="text" class="form-control" placeholder="g1_slot3"/>
-               <button class="btn btn-danger btn-sm"><i class="fas fa-minus"></i></button>
-          </div>
+          
 
 
               <hr/>
@@ -80,67 +69,10 @@ const App = () => {
           
         
             </div>
-    </div>
+        </div>
 
 
-
-
-      <div class="row">
-
-        <hr/>
-          <div class="col-md-5">
-            <label>#  Grupo</label>
-
-          </div>
- 
-          <div class="col-md-7">
-            <label>Slot</label>
-          </div>
-    </div>
-        <hr/>
-
-
-
-
-
-
-    <div class="row">
-
-
-      
-          <div class="col-md-5 form-control-div">
-      
-              <hr/>
-               <button class="btn btn-danger"><i class="fas fa-minus"></i></button>
-              <input type="text" class="form-control" placeholder="group2"/>
-          
-          </div>
-          
-
-
-            <div class="col-md-7">
-              
-           <div class="input-group-sm form-control-div1 mb-5">
-              <input type="text" class="form-control" placeholder="g2_slot1"/>
-               <button class="btn btn-danger btn-sm"><i class="fas fa-minus"></i></button>
-          </div>
-          <hr/>
-           <div class="input-group-sm form-control-div1 mt-5">
-            
-              <input type="text" class="form-control" placeholder="g2_slot2"/>
-               <button class="btn btn-danger btn-sm"><i class="fas fa-minus"></i></button>
-          </div>
-
-
-
-              <hr/>
-            <button class="btn btn-info btn-sm mt-5"><i class="fas fa-plus"></i>Add Slot</button>
-          
-        
-            </div>
-    </div>
-      
-    
+ */}
     
     </div>
 
